284c4d112a55c3b97252af6641e1552e2eb1713a
Thomas Cook <tlncook85@gmail.com>
2022-10-28 20:13:57 -0500

    added a few things

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React, { useState } from "react"
 function App() {
 const [count, setCount] = useState(0)
 
+function adjustCount(amount) {
+  setCount(currentCount => {
+    return currentCount + amount
+  })
+}
+
   return(
     <>
+      <div>Counter App</div>
       <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>Add</button>
-      <button onClick={() => setCount(count - 1)} disabled={count === 0}>Minus</button>
+      <button onClick={() => adjustCount(1)}>Add</button>
+      <button onClick={() => adjustCount(- 1)} disabled={count === 0}>Minus</button>
       <button onClick={() => setCount(0)} disabled={count === 0}>Reset</button>
     </>
   )
